3a3af4d4ef984d13758dfa041769d3ac146294af
Jorgedavyd <jorged.encyso@gmail.com>
2024-07-08 17:26:03 -0400

    solved: ci cd

--- requirements.sh
+++ requirements.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# Remove existing requirements.txt if it exists
 rm -f requirements.txt
-
-# Generate requirements.in from the lightorch directory
 pipreqs corkit/ --savepath=requirements.in
-
-# Compile requirements.in to requirements.txt
 pip-compile requirements.in
-
-# Clean up
 rm -f requirements.in
