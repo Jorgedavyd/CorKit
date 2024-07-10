128502ef7eb3959c406f055c95b8eae77eb6ac14
Jorgedavyd <jorged.encyso@gmail.com>
2024-07-10 12:50:45 -0400

    fixing ci cd and adding ci automation scripts

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -4,7 +4,7 @@ pip3 install pytest black pipreqs pip-tools
 rm -f requirements.txt
 pipreqs corkit --savepath=./requirements.in
 sed -i 's/==.*$//' requirements.in
-sed -i 's/skimage/scikit-image/' ./requirements_unique.in
+sed -i 's/skimage/scikit-image/' ./requirements.in
 sort requirements.in | uniq > requirements_unique.in
 pip-compile ./requirements_unique.in
 rm -f *.in
