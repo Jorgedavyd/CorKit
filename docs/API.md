5582a912f6224deb8588ade58aa7aa9154c9533d
Jorgedavyd <jorged.encyso@gmail.com>
2024-07-10 12:27:18 -0400

    fixing: ci cd, added: api and improved ci

---
+++ docs/API.md
@@ -0,0 +1,2 @@
+# CorKit API
+
--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -2,8 +2,8 @@
 python3 -m pip install --upgrade pip
 pip3 install pytest black pipreqs pip-tools
 rm -f requirements.txt
-pipreqs corkit --savepath=./requirements.in
-sed -i 's/skimage==0.0/scikit-image==0.15' ./requirements.in
+pipreqs corkit --ignore=venv --savepath=./requirements.in
+sed -i 's/skimage/scikit-image/' ./requirements.in
 pip-compile ./requirements.in
 rm -f ./requirements.in
 pip install -r requirements.txt
