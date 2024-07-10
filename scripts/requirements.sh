4d071232577a9b09a9240364f30aa0bddb7fadcc
Jorgedavyd <jorged.encyso@gmail.com>
2024-07-10 11:53:44 -0400

    added: scripts

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+python3 -m pip install --upgrade pip
+pip3 install pytest black pipreqs pip-tools
 rm -f requirements.txt
 pipreqs corkit --savepath=./requirements.in
 pip-compile ./requirements.in
 rm -f ./requirements.in
+pip install -r requirements.txt
+python3 update.py
