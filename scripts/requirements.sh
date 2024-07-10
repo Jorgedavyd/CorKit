360305b8ff6c477624b9dd59b36558a5b4080c4e
Jorgedavyd <jorged.encyso@gmail.com>
2024-07-10 12:11:58 -0400

    fixing: ci cd and pipreqs problems

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -3,6 +3,7 @@ python3 -m pip install --upgrade pip
 pip3 install pytest black pipreqs pip-tools
 rm -f requirements.txt
 pipreqs corkit --savepath=./requirements.in
+sed -i 's/skimage==0.0/scikit-image==0.15' ./requirements.in
 pip-compile ./requirements.in
 rm -f ./requirements.in
 pip install -r requirements.txt
