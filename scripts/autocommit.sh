3ff862704ceadcefb36cc2b36c14fdc9079bc2ca
Jorgedavyd <jorged.encyso@gmail.com>
2024-11-17 09:07:52 -0300

    ci/cd pipeline, scripts, pre commits

--- scripts/autocommit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-black .
-git config --global user.name 'Jorgedavyd'
-git config --global user.email 'jorged.encyso@gmail.com'
-git diff --exit-code || (git add . && git commit -m "Automatically formatted with black" && git push)
--- scripts/requirements.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3 -m pip install --upgrade pip
-pip3 install pytest black
-pip install --no-build-isolation -e .
-python3 update.py
